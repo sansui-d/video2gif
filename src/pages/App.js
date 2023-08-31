6e0d25f099313755cef7c4e794e97785e30e58e8
dengchuandong <sansui1021@163.com>
2023-08-31 22:40:46 +0800

    chroe: collating documents

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        123
+      </p>
+    </div>
+  );
+}
+
+export default App;
