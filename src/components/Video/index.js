e7206984ca4d63df189305f8af66d80cbab50b6b
dengchuandong <sansui1021@163.com>
2023-09-02 16:09:42 +0800

    add gif.js

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import GIF from 'gif.js'
+import { worker } from '../../utils/gif-worker'
 import './index.less';
 import video from '../../assets/video.mp4'
 
+const gif = new GIF({
+    workers: 2,
+    quality: 1,
+    workerScript: worker,
+});
+
 function Video(props) {
-    const { } = props
-    return <div className='video-content'>
-        <video controls autoPlay src={video}></video>
-    </div>
+    const { } = props;
+    const videoRef = useRef(null)
+    const timer = useRef(null);
+    const render = () => {
+        const cvs = document.getElementById("cvs");
+        const ctx = cvs.getContext("2d");
+        cvs.width = 500
+        cvs.height = 200
+        ctx.clearRect(0, 0, cvs.width, cvs.height);
+        ctx.drawImage(videoRef.current, 0, 0, cvs.width, cvs.height);
+        const img = document.createElement("img");
+        img.src = cvs.toDataURL("image/png");
+        img.onload = () => {
+            gif.addFrame(img, {
+                delay: 100,
+            });
+        };
+    }
+    const handleStart = () => {
+        timer.current = setInterval(render, 100);
+        videoRef.current.play();
+    }
+    const handleEnd = () => {
+        gif.on("finished", function (blob) {
+            console.log(blob)
+            const gifEl = document.getElementById("gifEl");
+            gifEl.src = URL.createObjectURL(blob);
+            gifEl.parentElement.setAttribute("download", "my.gif");
+            gifEl.parentElement.href = gifEl.src;
+        });
+        gif.render();
+        timer.current = null
+    }
+    return (
+        <div className='video-content'>
+            <video controls src={video} ref={videoRef}></video>
+            <canvas id="cvs"></canvas>
+            <div onClick={handleStart}>开始</div>
+            <div onClick={handleEnd}>结束</div>
+            <a><img src="" id="gifEl" /></a>
+        </div>
+    )
+
 }
 
 export default Video
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { video2gif } from '../utils/helper';
 import Nav from '../components/Nav';
 import Video from '../components/Video';
 import './App.less';
 
 function App() {
-  const handelClick = ()=>{
-    video2gif(document.getElementsByTagName('video')[0])
-  }
+
   return (
     <div className="video2gif">
       <Nav />
       <div className='video2gif-content'>
         <Video />
-        <div onClick={handelClick}>123</div>
       </div>
     </div>
   );
