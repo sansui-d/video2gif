9203d8cf94d1fb1b29c8ce6094f8dcd276618655
Dylan <71920152+sansui-d@users.noreply.github.com>
2023-09-14 15:43:36 +0800

    change video img

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from "react-redux"
 import video from '@assets/video.mp4'
-import img from '@assets/video.jpg'
 import './index.less';
 
 function Video() {
@@ -11,7 +10,7 @@ function Video() {
 
     return (
         <div className='video2gif-video'>
-            <video id='video2gif-video' poster={img} controls src={videoUrl || video}></video>
+            <video id='video2gif-video' controls src={videoUrl || video}></video>
         </div>
     )
 }
